bfee19ad15b0b51c91a65fbadb22e36cf0d8b281
WhiteStyx <luasshiro@gmail.com>
2023-09-14 13:09:57 +0700

    idle map

--- Dam daman/Assets/Script/Game.cs
+++ Dam daman/Assets/Script/Game.cs
@@ -4,14 +4,12 @@ using UnityEngine;
 
 public class Game : MonoBehaviour
 {
-    public GameObject chesspiece;
-
-    private GameObject[,] positions = new GameObjects[8, 8];
+    public GameObject slime;
 
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(chesspiece, new Vector3(0, 0, -1), Quaternion.identity);
+        Instantiate(slime, new Vector3(0, 0, -1), Quaternion.identity);
     }
 
     
